889e784ad2655c940b4b09b35add38f3c400e5cc
Kai Takac <kai.takac@gmail.com>
2016-03-11 13:12:26 +0100

    Work on super project

--- D4Muck_Ka0o0_android/SuperAwesomeAndroidApp.java
+++ D4Muck_Ka0o0_android/SuperAwesomeAndroidApp.java
@@ -1,7 +1,18 @@
+class Test {
+
 public void thisIsANewMethod(){
      System.out.println("Hello World!");
 }
 
 public static void main (String[] args){
      System.out.println("Hello World!");
+     int i = 0;
+     i++;
+     System.out.println(i);
+     System.out.println(++i);
+     System.out.println(i++);
+     Test t = new Test();
+     t.thisIsANewMethod();    
+}
+
 }
